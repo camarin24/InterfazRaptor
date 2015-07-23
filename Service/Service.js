b9fcc1282ab148a44de0ae47adea0733b2e44eb7
speakerblack <davidpaisa04@gmail.com>
2015-07-23 15:17:13 -0500

    Editando reproductor & Service js

--- Service/Service.js
+++ Service/Service.js
@@ -60,6 +60,17 @@ var service = {
 		})
 		return result;
 	},
+	getSimilarTrack: this.getSimilarTrack = function(mbid) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=track.getSimilar&api_key=0fbca00bcc89957dd5075f8c9661fe71&mbid="+mbid+"&format=json"
+		})
+		.done(function(data){
+			result = data['similartracks']['track'];
+		})
+		return result;
+	},
 	/*
 	  /$$$$$$  /$$ /$$                              
 	 /$$__  $$| $$| $$                              
@@ -124,6 +135,28 @@ var service = {
 		})
 		return result;
 	},
+	getAlbums: this.getAlbums = function(artist) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist="+artist+"&api_key=0fbca00bcc89957dd5075f8c9661fe71&format=json"
+		})
+		.done(function(data){
+			result = data['topalbums']['album'];
+		})
+		return result;
+	},
+	getTracks: this.getTracks = function(artist) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=artist.getTopTracks&artist="+artist+"&api_key=0fbca00bcc89957dd5075f8c9661fe71&format=json"
+		})
+		.done(function(data){
+			result = data['toptracks']['track'];
+		})
+		return result;
+	},
 	searchByArtist: this.searchByArtist = function(searchTerms) {
 		var result=-1;
 		$.ajax({
@@ -143,6 +176,63 @@ var service = {
 		})
 		.done(function(data){
 			result = data['artist'];
+		})
+		return result;
+	},
+	getSimilarArtist: this.getSimilarArtist = function(mbid) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&api_key=0fbca00bcc89957dd5075f8c9661fe71&mbid="+mbid+"&format=json"
+		})
+		.done(function(data){
+			result = data['similarartists']['artist'];
+		})
+		return result;
+	},
+	/*
+	 /$$$$$$$$                 
+	|__  $$__/                 
+	   | $$  /$$$$$$   /$$$$$$ 
+	   | $$ |____  $$ /$$__  $$
+	   | $$  /$$$$$$$| $$  \ $$
+	   | $$ /$$__  $$| $$  | $$
+	   | $$|  $$$$$$$|  $$$$$$$
+	   |__/ \_______/ \____  $$
+	                  /$$  \ $$
+	                 |  $$$$$$/
+	                  \______/ 
+	*/
+	getTrackByTag: this.getTrackByTag = function(tag) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag="+tag+"&api_key=0fbca00bcc89957dd5075f8c9661fe71&format=json"
+		})
+		.done(function(data){
+			result = data['toptracks']['track'];
+		})
+		return result;
+	},
+	getArtistByTag: this.getArtistByTag = function(tag) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag="+tag+"&api_key=0fbca00bcc89957dd5075f8c9661fe71&format=json"
+		})
+		.done(function(data){
+			result = data['topartists']['artist'];
+		})
+		return result;
+	},
+	getAlbumByTag: this.getAlbumByTag = function(tag) {
+		var result=-1;
+		$.ajax({
+			async: false,
+			url: "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag="+tag+"&api_key=0fbca00bcc89957dd5075f8c9661fe71&format=json"
+		})
+		.done(function(data){
+			result = data['topalbums']['album'];
 		})
 		return result;
 	}
